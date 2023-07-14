3b0b563d1d6294075abc6b777e54d36e806dc2b2
kdipto1 <kdipto@outlook.com>
2023-07-15 01:30:47 +0600

    redux toolkit added and tested with simple count state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { decrement, increment } from "./redux/features/count/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+
 function App() {
+  const { value } = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
   return (
     <>
-      <h1 className="text-2xl">check</h1>
+      <h1 className="text-2xl">{value}</h1>
+      <button onClick={() => dispatch(increment())} className="btn btn-square">
+        {" "}
+        Button
+      </button>
+      <button onClick={() => dispatch(decrement())} className="btn btn-square">
+        {" "}
+        Button
+      </button>
       <button className="btn btn-square"> Button</button>
     </>
   );
