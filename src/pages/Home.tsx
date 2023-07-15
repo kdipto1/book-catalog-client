229087ae1b15e3c07f3814898dd2d34e3a6c44d0
kdipto1 <kdipto@outlook.com>
2023-07-15 14:35:56 +0600

    All Books navigation button added in Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import HomeBooks from "../components/Home/HomeBooks";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className="mx-auto">
       <HomeBooks />
-      Home End
+      <Link className="btn " to="/allBooks">
+        All Books
+      </Link>
     </div>
   );
 }
